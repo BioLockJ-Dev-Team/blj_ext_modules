78659242e9398653837581315976494509a48580
Ivory Blakley <ieclabau@uncc.edu>
2019-05-13 13:14:07 -0400

    Start file for seq input class

---
+++ Tutorial01/src/basic/HelloSeqInput.java
@@ -0,0 +1,26 @@
+package basic;
+
+import biolockj.module.BioModuleImpl;
+
+public class HelloSeqInput extends BioModuleImpl
+{
+
+	@Override
+	public void checkDependencies() throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void executeTask() throws Exception
+	{
+		// Get input sequence files
+		// print number of lines in file
+		
+	}
+	
+	
+	
+
+}
