314fa0c87520e202d3ef61fd5bf98584010aaa4a
Ivory Blakley <ieclabau@uncc.edu>
2019-05-13 15:44:12 -0400

    Hello seq module works with simpleTest02

--- Tutorial01/src/basic/HelloSeqInput.java
+++ Tutorial01/src/basic/HelloSeqInput.java
@@ -1,8 +1,16 @@
 package basic;
 
-import biolockj.module.BioModuleImpl;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import biolockj.Config;
+import biolockj.Constants;
+import biolockj.module.SeqModuleImpl;
+import biolockj.util.BashScriptBuilder;
+import biolockj.util.SeqUtil;
 
-public class HelloSeqInput extends BioModuleImpl
+public class HelloSeqInput extends SeqModuleImpl
 {
 
 	@Override
@@ -15,12 +23,44 @@ public class HelloSeqInput extends BioModuleImpl
 	@Override
 	public void executeTask() throws Exception
 	{
-		// Get input sequence files
-		// print number of lines in file
-		
+		System.out.println( "Hello World wc!" );
+		saveFile();
+		BashScriptBuilder.buildScripts( this, buildScript( getInputFiles() ) );
+	}
+	
+	private void saveFile() throws Exception
+	{
+		File myfile = new File(getOutputDir(), "myFile.txt");
+		FileWriter fw = new FileWriter( myfile );
+		fw.write( "Hello Seq file tester!" );
+		fw.close();
+	}
+
+	@Override
+	public List<List<String>> buildScript( List<File> files ) throws Exception
+	{
+		final List<List<String>> data = new ArrayList<>();
+		for (File f:files) {
+			List<String> inner = new ArrayList<>();
+			File output = new File(getOutputDir(), f.getName() + "_wc.txt");
+			inner.add( RUN_WC + " " + f.getAbsolutePath() + " " + output.getAbsolutePath());
+			data.add( inner );
+		}
+		return data;
 	}
 	
 	
+	@Override
+	public List<String> getWorkerScriptFunctions() throws Exception {
+		final List<String> lines = super.getWorkerScriptFunctions();
+		lines.add( "function " + RUN_WC + "() {" );
+		lines.add("wc $1 > $2");
+		lines.add( "}" + RETURN );
+		return lines;
+	}
+	
+	
+	private static String RUN_WC = "runWC";
 	
 
 }
