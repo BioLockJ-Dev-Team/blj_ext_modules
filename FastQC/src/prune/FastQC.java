774a573ced8d1e1d13e45138eddaaa72d83de09a
Ivory <IvoryEC@gmail.com>
2019-06-09 17:34:02 -0400

    Don't set properties for the user; use what if given and advise improvements.

--- FastQC/src/prune/FastQC.java
+++ FastQC/src/prune/FastQC.java
@@ -4,27 +4,26 @@ package prune;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import biolockj.Config;
-import biolockj.module.ScriptModule;
-import biolockj.module.SeqModule;
-import biolockj.module.SeqModuleImpl;
-import biolockj.util.BashScriptBuilder;
-import biolockj.util.BioLockJUtil;
-import biolockj.util.DockerUtil;
+import biolockj.*;
+import biolockj.module.*;
+import biolockj.util.*;
 
 /**
  * Run FastQC to assess input sequence quality. This module is most efficient if
- * {@link biolockj.Config}.{@value this.SCRIPT_BATCH_SIZE} is a multiple of
- * {@link biolockj.Config}.{@value this.SCRIPT_NUM_THREADS}.
+ * {@link biolockj.Config}.{@value this.SCRIPT_BATCH_SIZE} is 1 and 
+ * {@link biolockj.Config}.{@value this.NUM_THREADS_PARAM} is a large 
+ * number that evenly divides the number of input files.
  * 
  * @author ivory
  *
  */
-public class FastQC extends SeqModuleImpl implements SeqModule
+public class FastQC extends SeqModuleImpl implements SeqModule, ScriptModule
 {
 
 	/**
-	 * Build the script lines for each file as a nested list.
+	 * Build the script lines for each worker script as a nested list.
+	 * With n threads, FasQC can process the n files at a time when they 
+	 * are passed with a single command (that is, one script line).
 	 *
 	 * @param files List of module input files
 	 * @return List of nested bash script lines.
@@ -34,9 +33,9 @@ public class FastQC extends SeqModuleImpl implements SeqModule
 	public List<List<String>> buildScript( List<File> files ) throws Exception
 	{
 		final List<List<String>> data = new ArrayList<>();
-		int batchSize = Config.requirePositiveInteger( this, SCRIPT_BATCH_SIZE );
+		int batchSize = Config.requirePositiveInteger( this, SCRIPT_NUM_THREADS );
 
-		if( DockerUtil.inDockerEnv() )
+		if( DockerUtil.inDockerEnv() )//TODO - docker mode doesn't always mean it should only have one batch
 		{
 			batchSize = files.size();
 		}
@@ -90,6 +89,13 @@ public class FastQC extends SeqModuleImpl implements SeqModule
 	{
 		super.checkDependencies();
 		checkParams();
+		if (Config.getPositiveInteger(this, SCRIPT_BATCH_SIZE) !=  1 
+				&& Config.getPositiveInteger(this, SCRIPT_NUM_THREADS) > 1 ) {
+			Log.info(this.getClass(), "The FastQC module runs most efficiently when " + SCRIPT_BATCH_SIZE 
+					+ " is set to 1, (currenlty set to [" + Config.getPositiveInteger(this, SCRIPT_BATCH_SIZE) + "])." +
+					"Each node is currently set to process [" + 
+					Config.getPositiveInteger(this, SCRIPT_NUM_THREADS) + "] files at a time because of the " + SCRIPT_NUM_THREADS + "] parameter.");
+		}
 	}
 
 	// TODO: some options may give a file path. (for example --contaminants and --adapters). In docker mode, this will
@@ -145,11 +151,6 @@ public class FastQC extends SeqModuleImpl implements SeqModule
 	@Override
 	public void executeTask() throws Exception
 	{
-		String propName = this.getClass().getSimpleName() + ScriptModule.SCRIPT_BATCH_SIZE.substring( ScriptModule.SCRIPT_BATCH_SIZE.indexOf( "." ) + 1 );
-		if( Config.getPositiveInteger(this, propName) == null )
-		{
-			Config.setConfigProperty( propName, "1" );
-		}
 		BashScriptBuilder.buildScripts( this, buildScript( getInputFiles() ) );
 	}
 
